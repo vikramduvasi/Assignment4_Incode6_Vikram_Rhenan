a942e651ecbb0bc215ece23d335517f86015463d
Rhenan Folco <rhenanfolco@hotmail.com>
2021-11-01 23:56:56 +1100

    Fix bcrypt import typo in index.js

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-const express = require('express')
-const bycrypt = require('bycryptjs')
-const morgan = require('morgan')
-const db = require('./database')
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const morgan = require('morgan');
+const db = require('./database');
 
-
-const app = express() // creating an instance of express
-const PORT = process.env.PORT || 4000
-
+const app = express(); // creating an instance of express
+const PORT = process.env.PORT || 4000;
 
 //middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(morgan('dev'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
-
-app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
