c5cdd4946513b0d3c43c9b19f084ddcd9c6e4c59
Vikram Duvasi <vikramduvasi30@gmail.com>
2021-11-10 16:18:37 +1100

    tested validations and middle ware need to discard this branch later

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+//validations
+const validation = require('../middleware/validationMiddleware')
+const userSchema = require('../validations/uservalidation')
+
+
 router.get('/', (req, res) => {
   res.render('pages/login');
 });
--- routes/signup.js
+++ routes/signup.js
@@ -3,38 +3,43 @@ const bcrypt = require('bcryptjs');
 const db = require('../database');
 const router = express.Router();
 
-const isValid = (value, regex) => {
-  return regex.test(value);
-};
+//validations
+const validation = require('../middleware/validationMiddleware')
+const userSchema = require('../validations/uservalidation')
+
+
+// const isValid = (value, regex) => {
+//   return regex.test(value);
+// };
 
 router.get('/', (req, res) => {
   res.render('pages/signup');
 });
 
-router.post('/', (req, res) => {
+router.post('/', validation(userSchema), (req, res) => {
   const { firstname, lastname, email, password, confirmPassword } = req.body;
   const cleanedEmail = email.toLowerCase().trim();
 
   // Regex
 
-  const firstNameRegex = /[A-Z][a-zA-Z][^#&<>\"~;$^%{}?]{0,20}$/;
-  const lastNameRegex = /[A-Z][a-zA-Z][^#&<>\"~;$^%{}?]{0,20}$/;
-  const emailRegex =
-    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+  // const firstNameRegex = /[A-Z][a-zA-Z][^#&<>\"~;$^%{}?]{0,20}$/;
+  // const lastNameRegex = /[A-Z][a-zA-Z][^#&<>\"~;$^%{}?]{0,20}$/;
+  // const emailRegex =
+  //   /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
-  // Password regex that requires at least 8 characters, one letter, and one number. No special characters accepted.
-  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  // // Password regex that requires at least 8 characters, one letter, and one number. No special characters accepted.
+  // const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
-  // Sign-up form validation
+  // // Sign-up form validation
 
-  if (!isValid(firstname, firstNameRegex))
-    return res.send('First name is not valid');
-  if (!isValid(lastname, lastNameRegex))
-    return res.send('Last name is not valid');
-  if (!isValid(cleanedEmail, emailRegex)) return res.send('Email is not valid');
-  if (!isValid(password, passwordRegex))
-    return res.send('Password is not valid');
-  if (password !== confirmPassword) return res.send("Passwords don't match");
+  // if (!isValid(firstname, firstNameRegex))
+  //   return res.send('First name is not valid');
+  // if (!isValid(lastname, lastNameRegex))
+  //   return res.send('Last name is not valid');
+  // if (!isValid(cleanedEmail, emailRegex)) return res.send('Email is not valid');
+  // if (!isValid(password, passwordRegex))
+  //   return res.send('Password is not valid');
+  // if (password !== confirmPassword) return res.send("Passwords don't match");
 
   // Check if email already exists in database
 
