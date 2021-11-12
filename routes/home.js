a419b781c24e9dbeb2ee142707f1e9c41eadce07
Rhenan Folco <rhenanfolco@hotmail.com>
2021-11-12 20:09:00 +1100

    Modify DB query and change ejs to display correct data on homepage

--- routes/home.js
+++ routes/home.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const db = require('../database');
 
-
 router.get('/', (req, res) => {
-    db.any('SELECT user_id, day, start_at, end_at FROM schedules;')
-        .then((schedules) => res.render('pages/homepage', { schedules }))
-        .catch((err) => console.log(err));
+  db.any(
+    'SELECT users.id, firstname, lastname, day, start_at, end_at FROM users INNER JOIN schedules ON users.id = user_id;'
+  )
+    .then((schedules) => res.render('pages/homepage', { schedules }))
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
